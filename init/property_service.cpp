ead98fd253d6818292a8baf21da6548f3b6e75fa
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-20 12:58:40 -0300

    init: Allow forcing custom build fingerprint [2/2]
    Change-Id: Iae1db7ad74d3cb14c2cd4405f7ede13a4444bc5a
    
    Former-commit-id: e139f1e09832c5117cddf551240442a8528c5c99

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -815,6 +815,19 @@ static const char *snet_prop_value[] = {
 	NULL
 };
 
+#ifdef TARGET_FORCE_BUILD_FINGERPRINT
+static const char *build_fingerprint_key[] = {
+    "ro.build.fingerprint",
+	"ro.system_ext.build.fingerprint",
+	"ro.vendor.build.fingerprint",
+	"ro.bootimage.build.fingerprint",
+	"ro.odm.build.fingerprint",
+	"ro.product.build.fingerprint",
+	"ro.system.build.fingerprint",
+	NULL
+};
+#endif
+
 static void workaround_snet_properties() {
     std::string build_type = android::base::GetProperty("ro.build.type", "");
 
@@ -830,6 +843,12 @@ static void workaround_snet_properties() {
             PropertySet(snet_prop_key[i], snet_prop_value[i], &error);
 	    }
     }
+
+    #ifdef TARGET_FORCE_BUILD_FINGERPRINT
+        for (int i = 0; build_fingerprint_key[i]; ++i) {
+            PropertySet(build_fingerprint_key[i], TARGET_FORCE_BUILD_FINGERPRINT, &error);
+        }
+    #endif
 
     // Restore the normal property override security after safetynet props have been set
     weaken_prop_override_security = false;
@@ -904,6 +923,7 @@ static void property_initialize_ro_product_props() {
     }
 }
 
+#ifndef TARGET_FORCE_BUILD_FINGERPRINT
 // If the ro.build.fingerprint property has not been set, derive it from constituent pieces
 static void property_derive_build_fingerprint() {
     std::string build_fingerprint = GetProperty("ro.build.fingerprint", "");
@@ -937,6 +957,7 @@ static void property_derive_build_fingerprint() {
                    << ")";
     }
 }
+#endif
 
 void PropertyLoadBootDefaults() {
     // TODO(b/117892318): merge prop.default and build.prop files into one
@@ -984,7 +1005,10 @@ void PropertyLoadBootDefaults() {
     weaken_prop_override_security = false;
 
     property_initialize_ro_product_props();
+
+#ifndef TARGET_FORCE_BUILD_FINGERPRINT
     property_derive_build_fingerprint();
+#endif
 
     if (android::base::GetBoolProperty("ro.persistent_properties.ready", false)) {
         update_sys_usb_config();
